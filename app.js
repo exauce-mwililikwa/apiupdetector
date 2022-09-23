151d3ac69a93be8757692efb2fee5051896ba47a
exauce-mwililikwa <exaucemwililikwa@gmail.com>
2022-09-23 12:13:24 +0200

    data

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(routes);
 
 // handling errors
 app.use((req,res,next)=>{
-    const error = new Error('Not found');
+    const error = new Error('Not found pas de connexion');
     error.status = 404;
     next(error);
 });
